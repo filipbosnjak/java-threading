2cbbd8c0cd45cd7cc43a30ecb30bd6aec11950a6
Filip.Bosnjak <filipo.bosnjak@gmail.com>
2023-07-06 17:59:43 +0200

    initial commit

---
+++ src/main/java/com/filip/javathreading/JavaThreadingApplication.java
@@ -0,0 +1,13 @@
+package com.filip.javathreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaThreadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filip/javathreading/JavaThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip.javathreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
